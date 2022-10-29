de9cffceabac89123e6961449c7dd5cb6c89c76a
Joseph Ibochi <josephibochi@gmail.com>
2022-10-29 05:07:18 +0100

    test

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -94,8 +94,7 @@ WSGI_APPLICATION = 'rest_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
     }
 }
 
