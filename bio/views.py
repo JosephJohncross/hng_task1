8f062bec544a046cffb9112ebbe088cb2424ba08
Joseph Ibochi <josephibochi@gmail.com>
2022-10-29 23:52:50 +0100

    change format of returned string

--- bio/views.py
+++ bio/views.py
@@ -1,6 +1,8 @@
 from rest_framework.response import Response
 from rest_framework import viewsets 
 from bio import  models, serializers
+from django.core import serializers as serialize
+from rest_framework import status
 
 
 class MyBioApiViewSet(viewsets.ModelViewSet):
@@ -8,7 +10,19 @@ class MyBioApiViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.MyBioSerializer
     queryset = models.MyBio.objects.all()
+    
 
-    def retrieve(self, request, *args, **kwargs):
-        # ret = super(StoryViewSet, self).retrieve(request)
-        return Response({'key': 'single value'})
+    def list(self, request, *args, **kwargs):
+        userExist = models.MyBio.objects.filter(id=1)
+        if userExist:
+            queryset = models.MyBio.objects.filter(id=1).values()
+            data = queryset[0]
+            data.pop('id')
+            response = data
+
+            if response is not None:
+                return Response(response, status.HTTP_200_OK)
+            else:
+                response = {}
+                return Response(response, status.HTTP_404_NOT_FOUND)
+    
